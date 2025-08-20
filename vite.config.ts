40f39931ae21a8b50a2ae732a686ae51ab498b11
Joel Vandenberg <jtvberg@gmail.com>
2025-08-20 15:05:31 -0500

    remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [sveltekit()],
 	server: {
 		host: '127.0.0.1',
 		port: 8181
